1b9266b1e17e875305a8031182ec559a80c3bffd
Shriyansh Raj <109217699+Shriyanshx@users.noreply.github.com>
2025-09-04 17:41:31 +0530

    test

--- lib/utils/shims/dart_ui_real.dart
+++ lib/utils/shims/dart_ui_real.dart
@@ -1,7 +1,5 @@
 // Copyright 2019 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-import 'dart:io' if(dart.library.html)'dart:html' as html;
+import 'dart:io' if(dart.library.html)'dart:html';
 export 'dart:ui' if(dart.library.html) 'dart:ui_web';
-
-dynamic get window => html.window;
